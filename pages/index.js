be4d483327ead9882260c0b4bd59578ab8a56b42
Boris Arkenaar <boris@arkenaar.com>
2018-08-13 19:12:27 +0200

    Make use of default styling in our page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
 import React from 'react';
 
+import '../shared/styles/_default.scss';
+import '../shared/styles/_fonts.scss';
+import '../shared/styles/_reset.scss';
+
 export default () => <h1>Welcome to Team Awesome! </h1>;
