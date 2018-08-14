d4702df9092bb7fca2e99c96cb1d2a388edf6b0b
Boris Arkenaar <boris@arkenaar.com>
2018-08-14 16:36:58 +0200

    Create navigation component

--- components/site-header/SiteHeader.jsx
+++ components/site-header/SiteHeader.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Navigation from '../navigation/Navigation';
+
 import './site-header.scss';
 
 class SiteHeader extends React.Component {
   state = { isMenuVisible: false };
+
   handleNavButtonClick = () => {
     this.setState((state) => ({
       isMenuVisible: !state.isMenuVisible
@@ -14,21 +17,23 @@ class SiteHeader extends React.Component {
     const { isMenuVisible } = this.state;
 
     return (
-      <header className={`
-        site-header
-        site-header--${isMenuVisible ? 'open' : 'closed'}
-      `}>
+      <header
+        className={`
+          site-header
+          site-header--${isMenuVisible ? 'open' : 'closed'}
+        `}
+      >
         <div className="site-header__mobile">
           <span className="site-header__title">team awesome</span>
           <button
             type="button"
-            className="site-header__nav-button"
-            onClick={ this.handleNavButtonClick }
+            className="site-header__navigation-button"
+            onClick={this.handleNavButtonClick}
           >
-            <span className="site-header__nav-button-label">Toggle Navigation</span>
+            <span className="site-header__navigation-button-label">Toggle Navigation</span>
           </button>
         </div>
-        <nav className="site-header__nav">Navigation</nav>
+        <Navigation className="site-header__navigation" />
       </header>
     );
   }
