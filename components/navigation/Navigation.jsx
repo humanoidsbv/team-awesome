66827cd773b0e9ab712817a4ff07b79fc718999a
Boris Arkenaar <boris@arkenaar.com>
2018-08-14 17:38:08 +0200

    Fill and style navigation component

--- components/navigation/Navigation.jsx
+++ components/navigation/Navigation.jsx
@@ -3,7 +3,48 @@ import React from 'react';
 import './navigation.scss';
 
 const Navigation = () => (
-  <div>Navigation</div>
+  <ul className="navigation">
+    <li className="navigation__item navigation__item--active">
+      <a
+        className="navigation__link"
+        href="/timesheets"
+      >
+        Timesheets
+      </a>
+    </li>
+    <li className="navigation__item">
+      <a
+        className="navigation__link"
+        href="/team-members"
+      >
+        Team members
+      </a>
+    </li>
+    <li className="navigation__item">
+      <a
+        className="navigation__link"
+        href="/projects"
+      >
+        Projects
+      </a>
+    </li>
+    <li className="navigation__item">
+      <a
+        className="navigation__link"
+        href="/clients"
+      >
+        Clients
+      </a>
+    </li>
+    <li className="navigation__item">
+      <a
+        className="navigation__link"
+        href="/documents"
+      >
+        Documents
+      </a>
+    </li>
+  </ul>
 );
 
 export default Navigation;
