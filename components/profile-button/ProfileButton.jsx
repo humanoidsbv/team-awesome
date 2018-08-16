daca7c1833aa1ba0453267142230c3799638dcba
Boris Arkenaar <boris@arkenaar.com>
2018-08-16 20:19:17 +0200

    Create styling for the desktop version
    Style the header of the app for desktop layout.
    Add dummy profile button component.

---
+++ components/profile-button/ProfileButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './profile-button.scss';
+
+const ProfileButton = () => (
+  <div className="profile-button">Profile Button</div>
+);
+
+export default ProfileButton;
