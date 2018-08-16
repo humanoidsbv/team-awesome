f6d340b4339c9bfc919a99d14d5ef26d762dc821
Boris Arkenaar <boris@arkenaar.com>
2018-08-16 21:18:55 +0200

    Implement and style the profile button
    The profile button is used on the desktop layout of the app.

--- components/profile-button/ProfileButton.jsx
+++ components/profile-button/ProfileButton.jsx
@@ -3,7 +3,24 @@ import React from 'react';
 import './profile-button.scss';
 
 const ProfileButton = () => (
-  <div className="profile-button">Profile Button</div>
+  <div className="profile-button">
+    <div className="profile-button__badge">
+      <img
+        className="profile-button__logo"
+        src="/static/humanoids-logo.png"
+        alt="Humanoids Logo"
+        width="75"
+        height="10"
+      />
+      <img
+        className="profile-button__photo"
+        src="/static/profile-photo-boris.png"
+        alt="Profile photo of Boris"
+        width="32"
+        height="32"
+      />
+    </div>
+  </div>
 );
 
 export default ProfileButton;
