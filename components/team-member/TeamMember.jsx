b888a52a98e82c4b19ae0350e9f479d131f0a64a
Boris Arkenaar <boris@arkenaar.com>
2018-09-04 12:35:36 +0200

    Add photo to team member row

--- components/team-member/TeamMember.jsx
+++ components/team-member/TeamMember.jsx
@@ -6,6 +6,13 @@ class TeamMember extends React.Component {
   render() {
     return (
       <div className="team-member">
+        <img
+          className="team-member__photo"
+          src="/static/profile-photo-boris.png"
+          alt="Profile of Boris"
+          width="50"
+          height="50"
+        />
         <div className="team-member__info">
           <div className="team-member__name">Boris Arkenaar</div>
           <div className="team-member__job">Frontend Post-Master Lead</div>
