15220140e24ad9659c594128d5990939ee3ed8d7
Boris Arkenaar <boris@arkenaar.com>
2018-09-05 13:20:23 +0200

    Add styling for extra info of team member

--- team-members/components/member/Member.jsx
+++ team-members/components/member/Member.jsx
@@ -6,16 +6,30 @@ class TeamMember extends React.Component {
   render() {
     return (
       <div className="member">
-        <img
-          className="member__photo"
-          src="/static/profile-photo-boris.png"
-          alt="Profile of Boris"
-          width="50"
-          height="50"
-        />
-        <div className="member__info">
-          <div className="member__name">Boris Arkenaar</div>
-          <div className="member__job">Frontend Post-Master Lead</div>
+        <div className="member__primary">
+          <img
+            className="member__photo"
+            src="/static/profile-photo-boris.png"
+            alt="Profile of Boris"
+            width="50"
+            height="50"
+          />
+          <div className="member__info">
+            <div className="member__value">Boris Arkenaar</div>
+            <div className="member__label">Frontend Post-Master Lead</div>
+          </div>
+        </div>
+        <div className="member__secondary">
+          <div className="member__extra-info">
+            <div className="member__info">
+              <div className="member__value">Humanoids</div>
+              <div className="member__label">Current employer</div>
+            </div>
+            <div className="member__info">
+              <div className="member__value">February 2018</div>
+              <div className="member__label">Starting date</div>
+            </div>
+          </div>
         </div>
       </div>
     );
